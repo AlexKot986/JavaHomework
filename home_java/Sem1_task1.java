918028664a6aecd3002c99d15d9858f89e725cdf
Alexershov86 <alexershov86@mail.ru>
2023-04-14 00:51:33 +0300

    ДЗ_1 Задача1

--- home_java/Sem1_task1.java
+++ home_java/Sem1_task1.java
@@ -1,5 +1,5 @@
 
-// *Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+// * 1. Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
 
 import java.util.Scanner;
 
@@ -7,17 +7,19 @@ class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input: ");
+
         int num = scanner.nextInt();
         scanner.close();
-        System.out.println("Output: " + subtractProductAndSum(num));
+        
+        System.out.println("Сумма от 0 до " + num + ": " + sumNums(num) + 
+        "\nФакториал " + num + "!: " + productNums(num));
     }
-    public static int subtractProductAndSum(int n) {
-        int product = 1, sum = 0;
-        for (;n != 0; n /= 10){
-            product *= (n % 10);
-            sum += (n % 10);
-        }
-        return product - sum;
+    public static int sumNums(int n){
+        return n * (n + 1) / 2;
+    }
+    public static int productNums(int n){
+        if (n == 0) return 1;
+        return n * productNums(n - 1);
     }
 }
 
