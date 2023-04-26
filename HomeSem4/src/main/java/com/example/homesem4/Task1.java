ec306fda2755f70b95b9c466eb48813edd91de24
Alexershov86 <alexershov86@mail.ru>
2023-04-26 23:58:37 +0300

    HomeWork4 Task1

--- HomeSem4/src/main/java/com/example/homesem4/Task1.java
+++ HomeSem4/src/main/java/com/example/homesem4/Task1.java
@@ -1,2 +1,29 @@
-package com.example.homesem4;public class Task1 {
+package com.example.homesem4;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Task1 {
+    /**
+     * 1)Вывести список на экран в перевернутом виде (без массивов и ArrayList)
+     * Пример:
+     * 1 -> 2->3->4
+     * Вывод:
+     * 4->3->2->1
+     */
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>(Arrays.asList(1, 2, 3, 4, 5));
+        System.out.println(list);
+
+        System.out.println(RevList(list));
+    }
+    public static LinkedList<Integer> RevList(LinkedList<Integer> list){
+
+        if(list.size() > 1) {
+            int number = list.pop();
+            RevList(list);
+            list.add(number);
+        }
+        return list;
+    }
 }
